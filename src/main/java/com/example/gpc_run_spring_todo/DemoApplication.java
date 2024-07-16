5b29e130efe7ef48a078d6a6baabe313743abe88
Baris Algun <76767449+berkaybarisalgun@users.noreply.github.com>
2024-07-16 13:58:01 +0300

    starters
    with code -> spring init \
      --dependencies=web,data-jpa,postgresql \
      --type=maven-project \
      --javaVersion=21 \
      cloud-run-todo

---
+++ src/main/java/com/example/gpc_run_spring_todo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gpc_run_spring_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gpc_run_spring_todo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gpc_run_spring_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
